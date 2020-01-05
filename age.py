94b2525aed3bd5d4c6e7599b0f8ac1ea748f2e4b
bryan lurer <blurer@pm.me>
2020-01-05 22:53:05 +0800

    capital

--- age.py
+++ age.py
@@ -7,7 +7,7 @@ age2050=int(age)-2019+2050
 ageNow=2019-int(age)
 
 print('')
-print('Hello,', name)
+print('Hello,', name.capitalize())
 print('You are', age , 'years old')
 print("If you were a dog, you would be", dog)
 print('You were born in', ageNow)
