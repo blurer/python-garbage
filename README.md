d69eb96a4a4a03d34276806f045463895c1a892a
bryan lurer <blurer@pm.me>
2020-01-05 22:48:51 +0800

    added age.py

---
+++ README.md
@@ -0,0 +1,15 @@
+### Python Education
+
+- age.py
+
+```
+ ⚙ bl@bl-mac  ~/Documents/projects/python-garbage   master  ./age.py 
+What is your name? Bryan
+What is your age? 32
+
+Hello, Bryan
+You are 32 years old
+If you were a dog, you would be 256
+You were born in 1987
+In 2050, you will be 63
+```
---
+++ age.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+name=input('What is your name? ')
+age=input('What is your age? ')
+dog=int(age) * 8
+age2050=int(age)-2019+2050
+ageNow=2019-int(age)
+
+print('')
+print('Hello,', name)
+print('You are', age , 'years old')
+print("If you were a dog, you would be", dog)
+print('You were born in', ageNow)
+print("In 2050, you will be", age2050)
